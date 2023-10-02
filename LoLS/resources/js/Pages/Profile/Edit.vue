1429e1ec888ef64dd83e8ebbf2a3550e03ab8ad1
sanyikak9 <sanyik92002@gmail.com>
2023-10-02 18:49:54 +0200

    Edit.vue Tab Names fixed.

--- LoLS/resources/js/Pages/Profile/Edit.vue
+++ LoLS/resources/js/Pages/Profile/Edit.vue
@@ -22,7 +22,7 @@ const tabs = {
     UpdatePasswordForm,
     DeleteUserForm
 }
-
+const tabNames = ref(["Update Profile","Update Password","Delete user"])
 
 </script>
 
@@ -35,12 +35,12 @@ const tabs = {
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
                 <button class="px-5"
-                    v-for="(_, tab) in tabs"
+                    v-for="(_, tab, index) in tabs"
                     :key="tab"
                     :class="['tab-button', { active: currentTab === tab }]"
                     @click="currentTab = tab"
                 >
-                    {{tab}}
+                    {{tabNames[index]}}
                 </button>
                 <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
                 <component :is="tabs[currentTab]" class="tab"></component>
