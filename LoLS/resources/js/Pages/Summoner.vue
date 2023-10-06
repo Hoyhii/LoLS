16e1fea6dab22878c7ed855d895278d7d1a8eff9
sanyikak9 <sanyik92002@gmail.com>
2023-10-06 16:50:47 +0200

    Styling summoner page /had to do it again bcs merging

--- LoLS/resources/js/Pages/Summoner.vue
+++ LoLS/resources/js/Pages/Summoner.vue
@@ -14,8 +14,8 @@ defineProps({
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
                 <nav>
 
-                    <InertiaLink :href="route('summoner.create')" class="px-5 text-lg font-medium text-gray-900 dark:text-gray-100">Add new Summoner</InertiaLink>
-                    <InertiaLink :href="route('summoner.edit')" class="px-5 text-lg font-medium text-gray-900 dark:text-gray-100">Edit summoners</InertiaLink>
+                    <InertiaLink :href="route('summoner.create')" class="px-5 text-lg font-medium text-white">Add new Summoner</InertiaLink>
+                    <InertiaLink :href="route('summoner.edit')" class="px-5 text-lg font-medium text-white  ">Edit summoners</InertiaLink>
 
                     <Link :href="route('summoner.create')" class="px-5 text-lg font-medium text-white">Add new Summoner</Link>
                     <Link :href="route('summoner.edit')" class="px-5 text-lg font-medium text-white">Edit summoners</Link>
